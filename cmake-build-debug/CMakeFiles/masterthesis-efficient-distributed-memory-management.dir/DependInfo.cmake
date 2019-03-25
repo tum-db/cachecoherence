85323a1404c1285a04ce3457cb5fb0209b9534ff
Magdalena Pröbstl <magdalena.proebstl@tum.de>
2019-03-25 12:40:31 +0100

    added generated makefile

---
+++ cmake-build-debug/CMakeFiles/masterthesis-efficient-distributed-memory-management.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/magdalena/Documents/Uni/5.Mastersemester/masterthesis-efficient-distributed-memory-management/main.cpp" "/Users/magdalena/Documents/Uni/5.Mastersemester/masterthesis-efficient-distributed-memory-management/cmake-build-debug/CMakeFiles/masterthesis-efficient-distributed-memory-management.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include/libibverbscpp"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/masterthesis-efficient-distributed-memory-management.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "libmasterthesis-efficient-distributed-memory-management.a"
+)
